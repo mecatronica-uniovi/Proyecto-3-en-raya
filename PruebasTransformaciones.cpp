85cae97811dea03632587cdd6c3aa5045f51a32b
[patri] <[patridevrs@gmail.com]>
2025-02-18 15:52:07 +0100

    Correccion de traslacion y CINEMÁTICA DIRECTA

---
+++ PruebasTransformaciones.cpp
@@ -0,0 +1,67 @@
+#include "robot_plano.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    matriz<float> M0(2,2), M1(2,2), M2(2,2), M3(2,2);
+    robot_plano robot;
+
+    cout<<"M0:"<<endl;
+    M0.print_matriz();
+
+    cout<<"M1:"<<endl;
+    M1.print_matriz();
+
+    cout<<"M2:"<<endl;
+    M2.print_matriz();
+
+    cout<<"M3:"<<endl;
+    M3.print_matriz();
+
+    M0.angulo_rot_g=0;
+    M1.angulo_rot_g=0;
+    M2.angulo_rot_g=90;
+    M3.angulo_rot_g=-90;
+
+    robot.R0=M0.matriz_rotacion();
+    robot.R01=M1.matriz_rotacion();
+    robot.R12=M2.matriz_rotacion();
+    robot.R23=M3.matriz_rotacion();
+
+    cout<<"R0:"<<endl;
+    robot.R0.print_matriz();
+
+    cout<<"R01:"<<endl;
+    robot.R01.print_matriz();
+
+    cout<<"R12:"<<endl;
+    robot.R12.print_matriz();
+
+    cout<<"R23:"<<endl;
+    robot.R23.print_matriz();
+
+    robot.determina_long_eslabones(0,125,125,90);
+
+    cout<<"Traslacion de S1:"<<endl;
+    robot.T01.print_matriz();
+
+    cout<<"Traslacion de S2:"<<endl;
+    robot.T12.print_matriz();
+
+    cout<<"Traslacion de S3:"<<endl;
+    robot.T23.print_matriz();
+
+    robot.rotacion_traslacion();
+
+    cout<<"Localización final de S1:"<<endl;
+    robot.P1.print_matriz();
+
+    cout<<"Localización final de S2:"<<endl;
+    robot.P2.print_matriz();
+
+    cout<<"Localización final de S3:"<<endl;
+    robot.P3.print_matriz();
+
+    return 0;
+}
--- robot_plano.h
+++ robot_plano.h
@@ -1,4 +1,5 @@
 #include "matriz.h"
+#include <cmath>
 
 #ifndef ROBOT_PLANO
 #define ROBOT_PLANO
@@ -7,6 +8,7 @@ class robot_plano{
     public:
         robot_plano();
 
+        float L0, ang_0;
         float L01, ang_1;
         float L12, ang_2;
         float L23, ang_3;
@@ -16,26 +18,30 @@ class robot_plano{
         matriz<float> P2;
         matriz<float> P3;
 
+        matriz<float> R0;
         matriz<float> R01;
         matriz<float> R12;
         matriz<float> R23;
 
+        matriz<float> T0;
         matriz<float> T01;
         matriz<float> T12;
         matriz<float> T23;
 
-        void determina_ang_eslabones(float ang_1, float ang_2, float ang_3);
-        void determina_long_eslabones(float l01, float l12, float l23);
+        void determina_ang_eslabones(float ang_0, float ang_1, float ang_2, float ang_3);
+        void determina_long_eslabones(float l0, float l01, float l12, float l23);
         void rotacion_traslacion();      
 };
 
 #endif
 
 robot_plano::robot_plano(){
+    L0=0;
     L01=0;
     L12=0;
     L23=0;
 
+    ang_0=0;
     ang_1=0;
     ang_2=0;
     ang_3=0;
@@ -45,35 +51,56 @@ robot_plano::robot_plano(){
     P2=matriz<float>(2,1);
     P3=matriz<float>(2,1);
 
+    R0=matriz<float>(2, 2);
     R01=matriz<float>(2, 2);  // Matriz 2x2
     R12=matriz<float>(2, 2);
     R23=matriz<float>(2, 2);
+
+    T0=matriz<float>(2,1);
     T01=matriz<float>(2, 1);  // Matriz 2x1
     T12=matriz<float>(2, 1);
     T23=matriz<float>(2, 1);
 
+    R0.angulo_rot_g=ang_0;
     R01.angulo_rot_g=ang_1;
     R12.angulo_rot_g=ang_2;
     R23.angulo_rot_g=ang_3;  
 }
 
-void robot_plano::determina_long_eslabones(float l01, float l12, float l23){
+void robot_plano::determina_long_eslabones(float l0,float l01, float l12, float l23){
+    L0=l0;
     L01=l01;
     L12=l12;
     L23=l23;
+
+    T0.set_valor(ang_0,0,0);
+    T0.set_valor(ang_0,1,0);
+
+    T01.set_valor(L01*cos(ang_1),0,0);
+    T01.set_valor(L01*sin(ang_1),1,0);
+
+    T12.set_valor(L12*cos(ang_2),0,0);
+    T12.set_valor(L12*sin(ang_2),1,0);
+
+    T23.set_valor(L23*cos(ang_3),0,0);
+    T23.set_valor(L23*sin(ang_3),1,0);
+
 }
 
-void robot_plano::determina_ang_eslabones(float ang1, float ang2, float ang3){
+void robot_plano::determina_ang_eslabones(float ang0, float ang1, float ang2, float ang3){
+    ang_0=ang0;
     ang_1=ang1;
     ang_2=ang2;
     ang_3=ang3;
 
+    R0.angulo_rot_g=ang_0;
     R01.angulo_rot_g=ang_1;
     R12.angulo_rot_g=ang_2;
     R23.angulo_rot_g=ang_3;
 }
 
 void robot_plano::rotacion_traslacion(){
+    R0.matriz_rotacion();
     R01.matriz_rotacion();
     R12.matriz_rotacion();
     R23.matriz_rotacion();
